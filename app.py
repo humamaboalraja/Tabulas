0529c3e9ba6e9cdd8630ef80cf3e30932de00659
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-10-22 03:51:41 +0200

    feat: Setting up osmnx, formatting output for each algorithm

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 import sys 
 
 from data.PriorityQueue import PriorityQueue
-from src.Dijkstra import dijkstraGraph
-from src.Astar import astarGraph
+from src.dijkstra import dijkstraGraph
+from src.astar import astarGraph
 from haversine import haversine, Unit
 
 
@@ -42,7 +42,33 @@ def create_app(test_config=None):
          dijkstraRouteColor = request.form['dijkstraRouteColor']
          dijkstraBgColor = request.form['dijkstraBgColor']
 
+         north, east, south, west = 0, 0, 0, 0
+         if startingPoint[0] >= endingPoint[0]:
+            north, south = startingPoint[0], endingPoint[0]
+         else:
+            north, south = endingPoint[0], startingPoint[0]
+         if startingPoint[1] >= endingPoint[1]:
+            east, west = startingPoint[1], endingPoint[1]
+         else:
+            east, west = endingPoint[1], startingPoint[1]
 
-         return render_template('index.html', astar="astar.jpg", dijkstra="dijkstra.jpg")
+         G = ox.graph_from_bbox(north + 0.01, south - 0.01, east + 0.01, west - 0.01, network_type="drive", simplify=True)
+         orig = ox.get_nearest_node(G, startingPoint)
+         destination = ox.get_nearest_node(G, endingPoint)
+
+         astarGraphObject = astarGraph(G)
+         dijkstraGraphObject = dijkstraGraph(G)
+
+         astarPath, astarVerticesExplored, astarEdgesExplored, astarCost = astarGraphObject.astar(orig, destination)
+         astarResult = f"Distance from [{request.form['startingPoint']} → {request.form['endingPoint']}] is {str(astarCost)} meters away 🎉 \n  A* 🗾 calculated/explored {str(astarVerticesExplored)} vertices and {str(astarEdgesExplored)} edges."
+         fig, ax = ox.plot_graph_route(G, astarPath, route_linewidth=4, node_size=0, route_color=astarRouteColor, save=True, bgcolor=astarBgColor, edge_color="#161616", filepath="static/img/astar.jpg", show=False, close=True)
+
+         dijkstraPath, dijkstraVerticesExplored, dijkstraEdgesExplored, dijkstraCost = dijkstraGraphObject.dijkstra(orig, destination)
+         dijkstraResult = f"Distance from [{request.form['startingPoint']} → {request.form['endingPoint']}] is {str(dijkstraCost)} meters away 🎉 | \n Dijkstra's 🗾 calculated/explored {str(dijkstraVerticesExplored)} vertices and {str(dijkstraEdgesExplored)} edges."
+         fig, ax = ox.plot_graph_route(G, dijkstraPath, route_linewidth=4, node_size=0, route_color=dijkstraRouteColor, save=True, bgcolor=dijkstraBgColor, edge_color="#161616", filepath="static/img/dijkstra.jpg", show=False, close=True)
+         shutil.rmtree('cache')
+
+
+         return render_template('index.html', astar="astar.jpg", dijkstra="dijkstra.jpg", astarResult = astarResult, dijkstraResult = dijkstraResult)
 
    return app
