939e34eed1284a2adb875fc758a0070125ae438d
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-11-19 14:02:40 +0100

    feat: PriorityQueue implementation on a heap time complexity

--- data/PriorityQueue.py
+++ data/PriorityQueue.py
@@ -6,8 +6,10 @@ class PriorityQueue:
     def empty(self):
         return not self.elements
     
-    def put(self, item, priority):
+    def put(self, item, priority): # O(log n)
         heapq.heappush(self.elements, (priority, item))
+        # When new element is added to the heap the order of elements gets adjusted to maintain heap structure.
     
-    def get(self):
+    def get(self): # O(log n)
         return heapq.heappop(self.elements)[1]
+        # This method removes and returns first element of the heap. When removing first element the order of other elements gets adjusted to maintain heap structure. If the heap was empty, then IndexError is raised.
